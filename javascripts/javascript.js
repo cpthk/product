a0c6b4dd1db1a0dfe0b79682df63adc4799fc803
Jeffrey <jeffrey@Jeffrey.(none)>
2012-02-06 20:14:14 -0800

    first commit

---
+++ javascripts/javascript.js
@@ -0,0 +1,27 @@
+
+
+$(function(){
+	var Main = Backbone.View.extend({
+		
+		
+		initialize: function () {
+			
+			
+			
+			
+		}
+
+
+		
+		
+		
+	
+	});
+	
+	var main = new Main();
+});
+
+$(document).ready(function(){
+
+});
+
